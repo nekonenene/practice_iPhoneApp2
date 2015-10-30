a9d8f530461f571a6bf2ae9e69f533c6d29c818a
Yokoe <yokoe.hnut@gmail.com>
2015-10-30 18:34:22 +0900

    start delegate test

---
+++ delegateTest/delegateTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  delegateTest
+//
+//  Created by Yokoe on 10/30/15.
+//  Copyright © 2015 Hatonekoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
